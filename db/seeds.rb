930dc7c48e1d72dbbdb9c18e2f7eec4e5d2ea1a8
thr3a <t@example.com>
2018-01-24 20:15:00 +0900

    モデルの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  group = Group.create(name: "group #{i}")
+  3.times do |k|
+    group.users << User.create(name: "user_#{k}", email: "#{k}@example.com")
+  end
+end
